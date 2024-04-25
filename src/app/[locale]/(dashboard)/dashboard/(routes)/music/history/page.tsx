b30371e911a7201723bb97a65c6971456cf2b29f
Lyndon Bethel <lyndonb1001@gmail.com>
2024-04-25 14:20:53 -0400

    correct link3

--- src/app/[locale]/(dashboard)/dashboard/(routes)/music/history/page.tsx
+++ src/app/[locale]/(dashboard)/dashboard/(routes)/music/history/page.tsx
@@ -1,15 +1,15 @@
-"use client"
-import { Music } from "lucide-react"
+"use client";
+import { Music } from "lucide-react";
 
-import { Heading } from "@/components/heading"
-import { buttonVariants } from "@/components/ui/button"
+import { Heading } from "@/components/heading";
+import { buttonVariants } from "@/components/ui/button";
 
-import { SubNav } from "@/components/sub-nav"
-import { EmptyPlaceholder } from "@/components/empty-placeholder"
-import Link from "next-intl/link"
-import { Icons } from "@/components/icons"
-import { cn } from "@/lib/utils"
-import { musicNavItems } from "../constants"
+import { SubNav } from "@/components/sub-nav";
+import { EmptyPlaceholder } from "@/components/empty-placeholder";
+import Link from "next/link";
+import { Icons } from "@/components/icons";
+import { cn } from "@/lib/utils";
+import { musicNavItems } from "../constants";
 
 const MusicHistoryPage = () => {
   return (
@@ -45,7 +45,7 @@ const MusicHistoryPage = () => {
         </EmptyPlaceholder>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MusicHistoryPage
+export default MusicHistoryPage;
