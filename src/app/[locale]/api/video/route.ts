d08b6c3c77a1ac4b983a85b5dbd8ee8e7af816ae
Lyndon Bethel <lyndonb1001@gmail.com>
2024-04-25 16:16:48 -0400

    correct link13

--- src/app/[locale]/api/video/route.ts
+++ src/app/[locale]/api/video/route.ts
@@ -1,41 +1,41 @@
-import Replicate from "replicate"
-import { auth } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
+import Replicate from "replicate";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit"
-import { checkSubscription } from "@/lib/subscription"
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
-import { env } from "@/env"
+import { env } from "@/env";
 
 // Specifies the maximum allowed duration for this function to execute (in seconds)
-export const maxDuration = 300
+export const maxDuration = 10;
 
 const replicate = new Replicate({
   auth: env.REPLICATE_API_TOKEN!,
-})
+});
 
 export async function POST(req: Request) {
   try {
-    const { userId } = auth()
-    const body = await req.json()
-    const { prompt } = body
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 })
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     if (!prompt) {
-      return new NextResponse("Prompt is required", { status: 400 })
+      return new NextResponse("Prompt is required", { status: 400 });
     }
 
-    const freeTrial = await checkApiLimit()
-    const isPro = await checkSubscription()
+    const freeTrial = await checkApiLimit();
+    const isPro = await checkSubscription();
 
     if (!freeTrial && !isPro) {
       return new NextResponse(
         "Free trial has expired. Please upgrade to pro.",
         { status: 403 }
-      )
+      );
     }
 
     const response = await replicate.run(
@@ -45,15 +45,15 @@ export async function POST(req: Request) {
           prompt,
         },
       }
-    )
+    );
 
     if (!isPro) {
-      await incrementApiLimit()
+      await incrementApiLimit();
     }
 
-    return NextResponse.json(response)
+    return NextResponse.json(response);
   } catch (error) {
-    console.log("[VIDEO_ERROR]", error)
-    return new NextResponse("Internal Error", { status: 500 })
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
